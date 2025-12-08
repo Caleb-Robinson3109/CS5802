5666eeefb12147e4091d7087f18a145a2f3dec30
caleb <calebrobinson3109@gmail.com>
2025-12-08 15:26:37 -0600

    added pathutil to cuda overaly

--- src/overlay/cuda.cu
+++ src/overlay/cuda.cu
@@ -1,11 +1,14 @@
 #include "bitmap.hpp"
 #include "overlay.hpp"
+#include "pathutil.hpp"
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::string;
+using std::vector;
 
 __global__ void kernel(const rgb* img1, const rgb* img2, rgb* result, int height, int width){
     int total_threads = blockDim.x * gridDim.x;
@@ -20,48 +23,48 @@ __global__ void kernel(const rgb* img1, const rgb* img2, rgb* result, int height
 
 int main()
 {
-    string filename1 = "img1.bmp";
-    string filename2 = "img2.bmp";
-    string output = "out.bmp";
+    vector<PathSet> paths = generatePaths();
 
-    bitmap img1(filename1);
-    bitmap img2(filename2);
+    for(PathSet currentPaths : paths){
+        bitmap img1(currentPaths.input1Path);
+        bitmap img2(currentPaths.input2Path);
 
-    int width = img1.get_width();
-    int height = img1.get_height();
-    int size = width * height;
+        int width = img1.get_width();
+        int height = img1.get_height();
+        int size = width * height;
 
-    rgb* img1_pixels = img1.get_pixels_copy();
-    rgb* img2_pixels = img2.get_pixels_copy();
+        rgb* img1_pixels = img1.get_pixels_copy();
+        rgb* img2_pixels = img2.get_pixels_copy();
 
-    rgb* result = new rgb[size];
+        rgb* result = new rgb[size];
 
-    rgb* d_img1_pixels;
-    rgb* d_img2_pixels;
-    rgb* d_result;
+        rgb* d_img1_pixels;
+        rgb* d_img2_pixels;
+        rgb* d_result;
 
-    int threads_per_block = 256;
-    int num_blocks = (size + threads_per_block - 1) / threads_per_block;
+        int threads_per_block = 256;
+        int num_blocks = (size + threads_per_block - 1) / threads_per_block;
 
-    cudaMalloc(&d_img1_pixels, size * sizeof(rgb));
-    cudaMalloc(&d_img2_pixels, size * sizeof(rgb));
-    cudaMalloc(&d_result, size * sizeof(rgb));
+        cudaMalloc(&d_img1_pixels, size * sizeof(rgb));
+        cudaMalloc(&d_img2_pixels, size * sizeof(rgb));
+        cudaMalloc(&d_result, size * sizeof(rgb));
 
-    cudaMemcpy(d_img1_pixels, img1_pixels, size * sizeof(rgb), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_img2_pixels, img2_pixels, size * sizeof(rgb), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_img1_pixels, img1_pixels, size * sizeof(rgb), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_img2_pixels, img2_pixels, size * sizeof(rgb), cudaMemcpyHostToDevice);
 
-    kernel<<<num_blocks, threads_per_block>>>(d_img1_pixels, d_img2_pixels, d_result, img1.get_height(), img1.get_width());
-    cudaDeviceSynchronize();
+        kernel<<<num_blocks, threads_per_block>>>(d_img1_pixels, d_img2_pixels, d_result, img1.get_height(), img1.get_width());
+        cudaDeviceSynchronize();
 
-    cudaMemcpy(result, d_result, size * sizeof(rgb), cudaMemcpyDeviceToHost);
+        cudaMemcpy(result, d_result, size * sizeof(rgb), cudaMemcpyDeviceToHost);
 
-    cudaFree(d_img1_pixels);
-    cudaFree(d_img2_pixels);
-    cudaFree(d_result);
+        cudaFree(d_img1_pixels);
+        cudaFree(d_img2_pixels);
+        cudaFree(d_result);
 
-    bitmap overlayed(width, height, result);
-    overlayed.save(output);
+        bitmap overlayed(width, height, result);
+        overlayed.save(currentPaths.outputPath);
 
-    delete[] result;
+        delete[] result;
+    }
     return 0;
 }
