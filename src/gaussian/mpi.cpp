edb3eafea08e1acfd2b753ecd1698531c011c062
Adam Seals <adam73seals@proton.me>
2025-12-07 21:29:39 -0600

    mpi gaussian implementation

---
+++ src/gaussian/mpi.cpp
@@ -0,0 +1,63 @@
+#include "bitmap.hpp"
+#include "gaussian.hpp"
+#include "pathutil.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+// NOTE: replace with:
+// #include <mpi.h>
+// on campus machines
+#include <openmpi-x86_64/mpi.h>
+
+int main(int argc, char* argv[]){
+
+    MPI_Init (&argc, &argv);
+    int nprocs;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    
+    std::vector<PathSet> paths = generatePaths();
+
+    // Cyclic partitioning
+    for (int i = rank; i < paths.size(); i += nprocs) {
+
+        PathSet currentPaths = paths[i];
+        std::cout << "rank " << rank << ": \n";
+        std::cout << "input 1 :" << currentPaths.input1Path << "\n";
+        std::cout << "input 2 :" << currentPaths.input2Path << "\n";
+        std::cout << "output :" << currentPaths.outputPath << "\n";
+        std::cout << std::endl;
+
+        bitmap img(currentPaths.input1Path);
+
+        int width = img.get_width();
+        int height = img.get_height();
+        rgb* img_pixels = img.get_pixels_copy();
+        rgb* result = new rgb[width * height];
+
+        int filter_size = 11;
+
+        double* gaussian_filter = create_gaussian_filter(filter_size);
+
+        // apply filter
+        int j = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                result[j] = apply_gaussian_filter(gaussian_filter, filter_size, img_pixels, height, width, x, y);
+                j++;
+            }
+        }
+
+        // output final bitmap
+        bitmap blurred(width, height, result);
+        blurred.save(currentPaths.outputPath);
+
+        delete[] result;
+    }
+    
+    MPI_Finalize();
+    return 0;
+}
