09e987e5edfd28ad279ea13fff5fcb62453fce0d
Adam Seals <adam73seals@proton.me>
2025-12-07 21:23:03 -0600

    fixed to not take an eternity

--- src/gaussian/serial.cpp
+++ src/gaussian/serial.cpp
@@ -15,24 +15,25 @@ int main(){
 
         int width = img.get_width();
         int height = img.get_height();
-        int size = width * height;
-
         rgb* img_pixels = img.get_pixels_copy();
+        rgb* result = new rgb[width * height];
 
-        rgb* result = new rgb[size];
+        int filter_size = 11;
 
-        double* gaussian_filter = create_gaussian_filter(size, 11);
+        double* gaussian_filter = create_gaussian_filter(filter_size);
 
         // apply filter
+        int i = 0;
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                apply_gaussian_filter(gaussian_filter, size, img_pixels, height, width, x, y);
+                result[i] = apply_gaussian_filter(gaussian_filter, filter_size, img_pixels, height, width, x, y);
+                i++;
             }
         }
 
         // output final bitmap
-        bitmap overlayed(width, height, result);
-        overlayed.save(currentPaths.outputPath);
+        bitmap blurred(width, height, result);
+        blurred.save(currentPaths.outputPath);
 
         delete[] result;
     }
